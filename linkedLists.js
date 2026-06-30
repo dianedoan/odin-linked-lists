aeb322e5214dcb1fcd1e2b3c960ba1cccccce9e7
Diane Doan <dianeddoan@gmail.com>
2026-06-30 13:24:37 -0600

    Add initial LinkedList and Node classes

---
+++ linkedLists.js
@@ -0,0 +1,13 @@
+class LinkedList {
+	constructor() {
+		this.head = null;
+		this.length = 0;
+	}
+}
+
+class Node {
+	constructor(value = null, nextNode = null) {
+		this.value = value;
+		this.nextNode = nextNode;
+	}
+}
