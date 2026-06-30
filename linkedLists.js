69964e80846c403959f7c3580fc4a398ad117c14
Diane Doan <dianeddoan@gmail.com>
2026-06-30 14:04:44 -0600

    Add append function

--- linkedLists.js
+++ linkedLists.js
@@ -1,7 +1,29 @@
 class LinkedList {
 	constructor() {
 		this.head = null;
-		this.length = 0;
+	}
+
+	// add a new node containing a value to end of list
+	append(value) {
+		// create new node
+		const newNode = new Node(value);
+
+		// if empty list, set new node as head
+		if (!this.head) {
+			this.head = newNode;
+		} else {
+			// start at head node
+			let current = this.head;
+
+			// find the end of the list
+			while (current.nextNode !== null) {
+				current = current.nextNode;
+			}
+
+			// last node in list points to new node
+			current.nextNode = newNode;
+		}
+
 	}
 }
 
