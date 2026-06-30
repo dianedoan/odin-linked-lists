484a7b5bc42aaa6e6cfe765c4717ea0af7654507
Diane Doan <dianeddoan@gmail.com>
2026-06-30 16:51:12 -0600

    Create tests for all functions

--- main.js
+++ main.js
@@ -3,10 +3,19 @@ import { LinkedList } from "./linkedLists.js";
 const list = new LinkedList();
 
 list.append("dog");
-list.append("cat");
 list.append("parrot");
 list.append("hamster");
 list.append("snake");
 list.append("turtle");
-
 console.log(list);
+list.prepend("cat");
+console.log(list);
+console.log(list.size());
+console.log(list.head());
+console.log(list.tail());
+console.log(list.at(4));
+console.log(list.pop());
+console.log(list);
+console.log(list.contains("snake"));
+console.log(list.findIndex("hamster"));
+console.log(list.toString());
